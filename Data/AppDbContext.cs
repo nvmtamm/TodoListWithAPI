51104219c34002eec59687a5da8fad73e0b22ac6
Nguyen Van Minh Tam <nvmtamm@gmail.com>
2026-04-18 22:47:35 +0700

    chore: Create C# structure with swagger tools and connect DB

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TodoListWithAPI.Models;
+
+namespace TodoListWithAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
